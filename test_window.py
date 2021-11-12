15b64eff0c269b149b2abc71badb485e2269c672
AlekseyGolikov <golikov.aleksey.1987@yandex.ru>
2021-11-12 17:00:43 +0300

    middle

--- test_window.py
+++ test_window.py
@@ -7,7 +7,16 @@ class App(Tk):
         self.geometry('200x150')
         self.x = 0
         self.label = Label(self,text=str(self.x))
+        self.label2 = Label(self,text='12345676890',justify=LEFT)
+        self.label3 = Label(self,text='123',justify=LEFT)
+
+        canvas = Canvas(self)
+        canvas.create_line(15,25,100,25, dash=(4, 2))
+        canvas.pack(fill=BOTH, expand=1)
+
         self.label.pack()
+        self.label2.pack()
+        self.label3.pack()
 
     def update(self):
         self.after(1000,self.update)
